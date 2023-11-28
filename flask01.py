8948943696bd6e281c4516ecea43cf62e74b813b
Jonatas Ramos <jonataswander@outlook.com>
2023-11-27 21:19:33 -0300

    Route Junction

--- flask01.py
+++ flask01.py
@@ -32,7 +32,7 @@ def prefix_remove(prefix, data):
 
 
 @app.route("/items", methods=["GET"])
-def get_all():
+def item_get_all():
 
     # Obtém todos os registros válidos de 'item'.
     # Request method → GET
@@ -87,7 +87,7 @@ def get_all():
 
 
 @app.route("/items/<int:id>", methods=["GET"])
-def get_one(id):
+def item_get_one(id):
 
     # Obtém um registro único de 'item', identificado pelo 'id'.
     # Request method → GET
@@ -133,7 +133,7 @@ def get_one(id):
 
 
 @app.route("/items", methods=["POST"])
-def create():
+def item_create():
     # Cadastra um novo registro em 'item'.
     # Request method → POST
     # Request endpoint → /items
@@ -196,7 +196,7 @@ def create():
 
 
 @app.route("/items/<int:id>", methods=["PUT", "PATCH"])
-def edit(id):
+def item_edit(id):
 
     # Edita um registro em 'item', identificado pelo 'id'.
     # Request method → PUT ou PATCH
@@ -247,7 +247,7 @@ def edit(id):
 
 
 @app.route("/items/<int:id>", methods=["DELETE"])
-def delete(id):
+def item_delete(id):
 
     # Marca, como apagado, um registro único de 'item', identificado pelo 'id'.
     # Request method → DELETE
@@ -288,8 +288,8 @@ def delete(id):
         return {"error": f"Erro inesperado: {str(error)}"}, 500
 
 
-@app.route("/owner_by_item/<int:id>", methods=["GET"])
-def get_items(id):
+@app.route("/items/all/<int:id>", methods=["GET"])
+def item_get_owners(id):
     try:
         conn = sqlite3.connect(database)
         conn.row_factory = sqlite3.Row
@@ -323,8 +323,8 @@ def get_items(id):
         return {"error": f"Erro inesperado: {str(error)}"}, 500
 
 
-@app.route("/items/search/<string:search>", methods=["GET"])
-def search_items(query):
+@app.route("/items/search/<string:query>", methods=["GET"])
+def item_search(query):
     try:
         conn = sqlite3.connect(database)
         conn.row_factory = sqlite3.Row
@@ -351,6 +351,192 @@ def search_items(query):
 
         else:
             return {"error": "Item não encontrado"}, 404
+
+    except sqlite3.Error as e:
+        return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
+
+    except Exception as error:
+        return {"error": f"Erro inesperado: {str(error)}"}, 500
+
+
+@app.route("/owners", methods=["GET"])
+def owner_get_all():
+    try:
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT * FROM owner WHERE owner_status != 'off' ORDER BY owner_name")
+        owners_rows = cursor.fetchall()
+        conn.close()
+        owners = []
+
+        for owner in owners_rows:
+            owners.append(dict(owner))
+
+        if owners:
+            new_owners = [prefix_remove('owner_', owner) for owner in owners]
+            return new_owners, 200
+
+        else:
+            return {"error": "Nenhum usuário encontrado"}, 404
+
+    except sqlite3.Error as e:
+        return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
+
+    except Exception as error:
+        return {"error": f"Erro inesperado: {str(error)}"}, 500
+
+
+@app.route("/owners/<int:id>", methods=["GET"])
+def owner_get_one(id):
+    try:
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT * FROM owner WHERE owner_id = ? AND owner_status != 'off'", (id,))
+        owner_row = cursor.fetchone()
+        conn.close()
+
+        if owner_row:
+            owner = dict(owner_row)
+            new_owner = prefix_remove('owner_', owner)
+            return new_owner, 200
+
+        else:
+            return {"error": "Usuário não encontrado"}, 404
+
+    except sqlite3.Error as e:
+        return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
+
+    except Exception as error:
+        return {"error": f"Erro inesperado: {str(error)}"}, 500
+
+
+@app.route("/owners", methods=["POST"])
+def owner_create():
+    try:
+        new_owner = request.get_json()
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT owner_email FROM owner WHERE owner_email = ? AND owner_status != 'off'", (new_owner['email'],))
+        owner_row = cursor.fetchone()
+
+        if owner_row:
+            conn.close()
+            return {"error": "Registro já existe"}, 200
+
+        else:
+            sql = "INSERT INTO owner (owner_name, owner_email, owner_password, owner_birth) VALUES (?, ?, ?, ?)"
+            sql_data = (
+                new_owner['name'],
+                new_owner['email'],
+                new_owner['password'],
+                new_owner['birth']
+            )
+            cursor.execute(sql, sql_data)
+            inserted_id = int(cursor.lastrowid)
+            conn.commit()
+            conn.close()
+            return {"success": "Registro criado com sucesso", "id": inserted_id}, 201
+
+    except json.JSONDecodeError as e:
+        return {"error": f"Erro ao decodificar JSON: {str(e)}"}, 500
+
+    except sqlite3.Error as e:
+        return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
+
+    except Exception as error:
+        return {"error": f"Erro inesperado: {str(error)}"}, 500
+
+
+@app.route("/owners/<int:id>", methods=["PUT", "PATCH"])
+def owner_edit(id):
+    try:
+        owner_json = request.get_json()
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT owner_id FROM owner WHERE owner_id = ? AND owner_status != 'off'", (id,))
+        owner_row = cursor.fetchone()
+
+        if owner_row:
+            set_clause = ', '.join(
+                [f"owner_{key} = ?" for key in owner_json.keys()])
+
+            sql = f"UPDATE owner SET {set_clause} WHERE owner_id = ? AND owner_status = 'on'"
+            cursor.execute(sql, (*owner_json.values(), id))
+            conn.commit()
+            conn.close()
+            return {"success": "Registro atualizado com sucesso", "id": id}, 201
+
+        else:
+            conn.close()
+            return {"error": "Registro não existe"}, 404
+
+    except json.JSONDecodeError as e:
+        return {"error": f"Erro ao decodificar JSON: {str(e)}"}, 500
+
+    except sqlite3.Error as e:
+        return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
+
+    except Exception as error:
+        return {"error": f"Erro inesperado: {str(error)}"}, 500
+
+
+@app.route("/owners/<int:id>", methods=["DELETE"])
+def owner_delete(id):
+    try:
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT owner_id FROM owner WHERE owner_id = ? AND owner_status != 'off'", (id,))
+        owner_row = cursor.fetchone()
+
+        if owner_row:
+            cursor.execute(
+                "UPDATE owner SET owner_status = 'off' WHERE owner_id = ?", (id,))
+            conn.commit()
+            conn.close()
+            return {"success": "Registro apagado com sucesso", "id": id}, 200
+
+        else:
+            conn.close()
+            return {"error": "Usuário não existe"}, 404
+
+    except sqlite3.Error as e:
+        return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
+
+    except Exception as error:
+        return {"error": f"Erro inesperado: {str(error)}"}, 500
+
+
+@app.route("/owners/items/<int:id>", methods=["GET"])
+def owner_get_items(id):
+    try:
+        conn = sqlite3.connect(database)
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT * FROM item WHERE item_status != 'off' AND item_owner = ? ORDER BY item_date DESC", (id,))
+        items_rows = cursor.fetchall()
+        conn.close()
+        items = []
+
+        for item in items_rows:
+            items.append(dict(item))
+
+        if items:
+            new_items = [prefix_remove('item_', item) for item in items]
+            return new_items, 200
+
+        else:
+            return {"error": "Usuário não encontrado"}, 404
 
     except sqlite3.Error as e:
         return {"error": f"Erro ao acessar o banco de dados: {str(e)}"}, 500
